53a87b0fc5eb824911710046f931512f3de1a652
윤태현 <yoonth0919@gmail.com>
2023-10-01 03:03:27 +0900

    2023-10-01 수정

--- src/components/main/Skill.jsx
+++ src/components/main/Skill.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'styles/skill.css';
 import image2 from 'assets/images/2.png';
+import image3 from 'assets/images/3.png';
 
 const Skill = () => {
     return (
@@ -12,10 +13,11 @@ const Skill = () => {
                     <h4>Front-End</h4>
                     <div>
                         <ul>
-                            <li>HTML/CSS</li>
-                            <li>Pug</li>
-                            <li>JavaScript</li>
+                            <li>HTML/CSS/JavaScript</li>
                             <li>jQuery</li>
+                            <li>React</li>
+                            <li>Redux Toolkit</li>
+                            <li>Style Components</li>
                         </ul>
                     </div>
                 </div>
@@ -24,8 +26,9 @@ const Skill = () => {
                     <h4>Back-End</h4>
                     <div>
                         <ul>
-                            <li>Flask</li>
                             <li>Node.js</li>
+                            <li>ExpressJS</li>
+                            <li>Flask</li>
                             <li>AWS (EC2, S3, SES 등)</li>
                         </ul>
                     </div>
@@ -44,24 +47,63 @@ const Skill = () => {
 
             <hr></hr>
 
+            <p className="additional" style={{ margin: "10px" }}>팀 프로젝트입니다.</p>
+            <div className="info2-project" style={{ marginBottom: "10px" }}>
+                <img className="project-img3" src={image3} alt="이미지3" loading='lazy-loading'></img>
+                <div className="project-info">
+                    <h3 style={{ marginTop: "0" }}>웅진씽크빅 웹 빌더</h3>
+                    <div className="use-skill">
+                        <span>HTML / CSS / JS</span>
+                        <span>React</span>
+                        <span>Redux-toolkit</span>
+                        <span>Node.js</span>
+                        <span>ExpressJS</span>
+                        <span>MySQL</span>
+                        <span>Netlify / ngrok</span>
+                    </div>
+                    <div className='description'>
+                        <p>개발자나 퍼블리셔가 아닌 누구나 쉽고 빠르게 상세 페이지를 제작 할 수 있는 웹 빌더입니다.</p>
+                    </div>
+                    <div className='description'>
+                        <p>현재는 백엔드 서버가 ngrok 테스트만 진행 <br /> AWS EC2, RDS로 배포 진행 중</p>
+                    </div>
+                    <ul>
+                        <li>팀 구성 - 4인</li>
+                        <li><a href="https://woongjin-web-builder.netlify.app/">https://woongjin-web-builder.netlify.app/</a></li>
+                        <li><a href="https://github.com/yoonth95/Web-Builder">https://github.com/yoonth95/Web-Builder</a></li>
+                    </ul>
+                </div>
+            </div>
+                
+            <hr></hr>
+
             <p className="additional" style={{ margin: "10px" }}>개인 프로젝트입니다.</p>
-            <div className="info2" style={{ marginBottom: "100px" }}>
-                <img src={image2} alt="이미지2" loading='lazy-loading'></img>
+            <div className="info2-project" style={{ marginBottom: "10px" }}>
+                <img className="project-img2" src={image2} alt="이미지2" loading='lazy-loading'></img>
                 <div className="project-info">
                     <h3 style={{ marginTop: "0" }}>계란 요리 성격 유형 검사</h3>
                     <div className="use-skill">
                         <span>HTML/CSS</span>
                         <span>Vaniila JS</span>
+                        <span>React</span>
                         <span>Firebase</span>
                     </div>
-                    <div style={{ margin: "10px 0" }}>
+                    <div className="description">
                         <p>성격 유형 검사를 통해 계란 요리와 매칭 시켜서 알려준다.</p>
                         <p>기획부터 시작해서 개발 및 배포까지 1인 개발</p>
                     </div>
                     <ul>
                         <li>팀 구성 - 개인</li>
-                        <li><a href="https://egg-type.web.app/">https://egg-type.web.app/</a></li>
-                        <li><a href="https://github.com/yoonth95/EBTI">https://github.com/yoonth95/EBTI</a></li>
+                        <li style={{marginTop: '20px'}}>React</li>
+                        <ul style={{paddingTop: '0'}}>
+                            <li><a href="https://egg-type.web.app/">https://egg-type.netlify.app/</a></li>
+                            <li><a href="https://github.com/yoonth95/EBTI-React">https://github.com/yoonth95/EBTI-React</a></li>
+                        </ul>
+                        <li style={{marginTop: '20px'}}>VanillaJS, Firebase</li>
+                        <ul style={{paddingTop: '0'}}>
+                            <li><a href="https://egg-type.web.app/">https://egg-type.web.app/</a></li>
+                            <li><a href="https://github.com/yoonth95/EBTI">https://github.com/yoonth95/EBTI</a></li>
+                        </ul>
                     </ul>
                 </div>
             </div>
